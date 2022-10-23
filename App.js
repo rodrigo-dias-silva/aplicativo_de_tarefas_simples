8a6b53382528115146767cf799ed8ab1b7c205a9
rodrigo-dias-silva <rodrigo-cleiton@hotmail.com>
2022-10-23 03:37:59 -0300

    atualizaçao do projeto, pré funcionalidades dos itens no FlatList e criaçao de uma tabela temporaria

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import Login from './src/components/Login';
+import TaskList from './src/components/TaskList';
+
+let tasks = [
+  { key: '1', task: 'Pagar o agiota' },
+  { key: '2', task: 'Pagar o travesti' },
+  { key: '3', task: 'Fazer teste de HIV' }
+]
 
 export default function App() {
   const [user, setUser] = useState(null)
 
-  if (!user) {
-    return <Login changeStatus={(user) => setUser(user)} />
+  const [newTask, setNewTask] = useState('')
+
+  function handleDelete(key) {
+    console.log(key);
   }
+
+  function handleEdit(data) {
+    console.log(data);
+  }
+
+  // if (!user) {
+  //   return <Login changeStatus={(user) => setUser(user)} />
+  // }
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="inverted" />
-      <Text>Dentro da tela de tarefas</Text>
+      <View style={styles.subContainer}>
+        <View style={styles.containerInput}>
+          <TextInput
+            style={styles.input}
+            placeholder="O que vai fazer hoje?"
+            value={newTask}
+            onChangeText={(text) => setNewTask(text)}
+            placeholderTextColor="#fff"
+          />
+          <TouchableOpacity style={styles.btnAdd}>
+            <Text style={{ fontSize: 25 }}>+</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={tasks}
+          keyExtractor={item => item.key}
+          renderItem={({ item }) => (
+            <TaskList
+              data={item}
+              deleteItem={handleDelete}
+              editItem={handleEdit}
+            />
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -22,9 +62,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 10,
-    backgroundColor: '#fff',
+    backgroundColor: '#121214',
+    // alignItems: 'center',
+  },
+  subContainer: {
+    marginHorizontal: 15
+  },
+  containerInput: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    flex: 1,
+    color: '#fff',
+    backgroundColor: '#4f4f4f',
+    marginVertical: 10,
+    padding: 15,
+    height: 50,
+    borderRadius: 5,
+    fontSize: 16
+  },
+  btnAdd: {
+    height: 50,
+    width: 50,
+    alignItems: 'center',
+    backgroundColor: '#81d8f7',
+    borderRadius: 5,
+    marginLeft: 10,
+    justifyContent: 'center'
   },
 });
